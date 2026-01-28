cdc5b2b85e25258d954f2470ba6bcbcb2267e938
Eduardo Rosa Tedesco <eduardo.tedesco19@gmail.com>
2026-01-28 16:51:07 -0300

    Conexão da Api do Open Weather, permitindo pesquisar atributos do clima. Estudo de uso de API, e tradução para JSON, assim como o uso de promisses.

---
+++ scripts.js
@@ -0,0 +1,48 @@
+/*Lógica do script
+
+-fluxo básico-
+.captar quando o botao foi clicado x
+.pegar nome da cidade e guardar em variavel x
+.enviar o nome da cidade para a API x
+.pegar os dados da API e guardar em variaveis x
+.atualizar a interface com os novos dados
+
+-fluxo de voz-
+.descobrir quando o botão mic foi clicado
+.ouvir e pegar a trasncricao do que foi dito
+.enviar a transcricao para o servidor de API
+.pegar a resposta e mostrar
+
+-fluxo da IA-
+.pegar os dados da cidade selecionada
+.enviar os dados para o servidor da IA
+.pegar a resposta da IA e mostrar na interface
+
+*/
+
+let chave = "8e6b8acf839224920f0f0bd17562f250"; //Chave da API do OpenWeatherMap para a pesquisa da previsão do tempo
+
+async function cliqueBotao(){ /*async e await andam juntos, tornando a ação de buscar os dados assíncrona, indicam promisses(promessa de que no fututro vai ter resultado)*/
+    let cidade = document.querySelector(".inputCidade").value;
+    let endereco = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chave}&lang=pt_br&units=metric`;
+    /*template string em que pode botar outras variáveis dentro, como cidade e chave*/
+
+    let caixa = document.querySelector(".caixaMedia");
+
+    //Precisa avisar o Javascript que vai fazer uma requisição para um servidor externo
+    //traduzir a resposta do servidor para um formato que o Javascript entenda (JSON)
+
+    let respostaServidor = await fetch(endereco);/*await faz a função esperar até que a promessa seja resolvida*/
+    let dadosJson = await respostaServidor.json();/*.json() é uma função que pega a resposta do servidor e traduz para JSON*/
+
+    //Math.floor(dadosJson.main.temp) arredonda para baixo a temperatura
+    caixa.innerHTML = `
+        <h2 class="cidade">${dadosJson.name}</h2>
+        <p class="temperatura">${Math.floor(dadosJson.main.temp)} °C</p>
+        <img class="icone" src="https://openweathermap.org/img/wn/${dadosJson.weather[0].icon}.png" alt="Ícone do clima">
+        <p class="umidade">Umidade: ${dadosJson.main.humidity} %</p>
+        <button class="botaoIA">Sugestão de roupa</button>
+        <p class="respostaIA">Resposta da IA</p>
+    `
+    /*O icone é montado através da url disponibilizada no site juntamente com o código do ícone */
+}
