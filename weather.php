bf7896ffe4ffe1eb01c23e7294ee67dab1c50b9d
Eduardo Rosa Tedesco <eduardo.tedesco19@gmail.com>
2026-01-29 16:27:38 -0300

    Mudanças na forma de captar a cidade selecionada, o atributo urlencode permite tratar espaços e caracteres especiais para montar url.

--- weather.php
+++ weather.php
@@ -1,6 +1,6 @@
 <?php 
     require "config.php";//importa o arquivo de configuração onde está a chave da api, usar require pois se config.php não existir ele para a execução
-    $cidade = $_GET['cidade'];//pega a cidade passada por parâmetro na url pelo JS
+    $cidade = urlencode($_GET['cidade']);//pega a cidade passada por parâmetro na url pelo JS, urlencode para tratar espaços e caracteres especiais
     $url = "https://api.openweathermap.org/data/2.5/weather?q=".$cidade."&appid=".chave."&lang=pt_br&units=metric";
 
     echo file_get_contents($url);//isso retorna o json da api e imprime como resposta
